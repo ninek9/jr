7c412241380a70a1d77d893f957d2ab095453dd0
Eric Zanol <ezanol@gmail.com>
2011-02-06 13:01:22 -0800

    trying out branching

--- mod/jockroster_theme/views/default/account/forms/register.php
+++ mod/jockroster_theme/views/default/account/forms/register.php
@@ -20,7 +20,7 @@ if (($_SESSION['user']->admin) && ($vars['show_admin'])) {
 		$admin_option = true;
 }
 
-$form_body = "<p><label>" . elgg_echo('name') . "</label>" . elgg_view('input/text' , array('internalname' => 'regname', 'class' => "general-textarea", 'value' => $name, 'autocomplete' => 'off')) . "<br />";	
+$form_body = "<p class=\"testing\"><label>" . elgg_echo('name') . "</label>" . elgg_view('input/text' , array('internalname' => 'regname', 'class' => "general-textarea", 'value' => $name, 'autocomplete' => 'off')) . "<br />";	
 $form_body .= "<label>" . elgg_echo('email') . "</label>" . elgg_view('input/text' , array('internalname' => 'regemail', 'class' => "general-textarea", 'value' => $email, 'autocomplete' => 'off')) . "<br />";
 $form_body .= "<label>" . elgg_echo('username') . "</label>" . elgg_view('input/text' , array('internalname' => 'regusername', 'class' => "general-textarea", 'value' => $username, 'autocomplete' => 'off')) . "<br />";
 $form_body .= "<label>" . elgg_echo('password') . "</label>" . elgg_view('input/password' , array('internalname' => 'regpassword', 'class' => "general-textarea", 'autocomplete' => 'off')) . "<br />";
